372b10eb95273e25d6f5c23d06f132fde88db7cb
kantav.05 <kantav.05@A321-12>
2022-03-15 09:33:58 +0100

    Metody11

--- Metody11/Form1.cs
+++ Metody11/Form1.cs
@@ -15,6 +15,33 @@ namespace Metody11
         public Form1()
         {
             InitializeComponent();
+            textBox1.Text = "pes kočka   vlk liška";
+        }
+
+        private int PocetSlov(string text,out string upravenyText)
+        {
+            int pocetSlov = 0;
+            char[] separator = { ' ' };
+            string[] slova= text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            pocetSlov = slova.Count();
+            int i = 0;
+            while (i<text.Length)
+            {
+                if (char.IsDigit(text[i]))
+                {
+                    text = text.Remove(i, 1);
+                }
+                else i++;
+            }
+            upravenyText = text;
+            return pocetSlov;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string upraveno;
+            int pocetSlov = PocetSlov(textBox1.Text,out upraveno);
+            MessageBox.Show("Počet slov: "+pocetSlov+"\nŘetězec bez čísel: " + upraveno);
         }
     }
 }
