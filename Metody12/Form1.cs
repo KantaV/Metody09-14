2b03e1f549319bd7b24486e6632b676777c1c449
kantav.05 <kantav.05@A321-12>
2022-03-18 08:13:14 +0100

    Metody12

--- Metody12/Form1.cs
+++ Metody12/Form1.cs
@@ -16,5 +16,39 @@ namespace Metody12
         {
             InitializeComponent();
         }
+
+        private bool ObsahujeSlovo(string text, out string nejdelsiSlovo, out string nejkratsiSlovo)
+        {
+            char[] oddelovac = { ' ' };
+            string[] slova = text.Split(oddelovac, StringSplitOptions.RemoveEmptyEntries);
+            nejkratsiSlovo = "";
+            nejdelsiSlovo = "";
+           
+            bool obsahujeSlovo = slova.Length > 0;
+
+            if (obsahujeSlovo)
+            {
+                nejdelsiSlovo = slova[0];
+                nejkratsiSlovo = slova[0];
+
+                foreach (string slovo in slova)
+                {
+                    if (slovo.Length < nejkratsiSlovo.Length) nejkratsiSlovo = slovo;
+                    if (slovo.Length > nejdelsiSlovo.Length) nejdelsiSlovo = slovo;
+                }
+            }
+            return obsahujeSlovo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            string nejdelsi, nejkratsi;
+            if (ObsahujeSlovo(text, out nejdelsi, out nejkratsi))
+            {
+                MessageBox.Show("Řetězec obsahuje alespoň jedno slovo\nNejdelší slovo - " + nejdelsi + "\nNejkratší slovo - " + nejkratsi);
+            }
+            else MessageBox.Show("Řetězec neobsahuje alespoň jedno slovo");
+        }
     }
 }
